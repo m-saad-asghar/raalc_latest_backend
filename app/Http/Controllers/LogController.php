ba2c089b9cb76b3ad25b37443ef6335e3faac88f
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-04-18 12:19:10 +0400

    filters

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -40,30 +40,52 @@ class LogController extends Controller
     public function getLogs(Request $request)
     {
         $perPage = $request->get('per_page', 15);
-        $origin = $request->get('origin');
-
-        $query = DB::table('logs');
-
-        if ($origin) {
-            $query->where('origin', 'LIKE', "%$origin%");
-        }
-
         $currentPage = $request->get('page', 1);
         $offset = ($currentPage - 1) * $perPage;
-
+    
+        // Retrieve all filters
+        $origin = $request->get('origin');
+        $type = $request->get('type');
+        $source = $request->get('source');
+        $ipAddress = $request->get('ip_address');
+        $pageUrl = $request->get('page_url');
+    
+        // Build the query
+        $query = DB::table('logs');
+    
+        if ($origin) {
+            $query->where('origin', $origin);
+        }
+    
+        if ($type) {
+            $query->where('type', $type);
+        }
+    
+        if ($source) {
+            $query->where('source', $source);
+        }
+    
+        if ($ipAddress) {
+            $query->where('ip_address', 'LIKE', "%$ipAddress%");
+        }
+    
+        if ($pageUrl) {
+            $query->where('page_url', 'LIKE', "%$pageUrl%");
+        }
+    
         $total = $query->count();
-
+    
         $data = $query->select('id', 'page_url', 'ip_address', 'created_at', 'origin', 'type', 'source')
                       ->orderBy('created_at', 'desc')
                       ->offset($offset)
                       ->limit($perPage)
                       ->get();
-
+    
         return response()->json([
-            'current_page' => (int)$currentPage,
-            'per_page' => (int)$perPage,
+            'current_page' => (int) $currentPage,
+            'per_page' => (int) $perPage,
             'total' => $total,
-            'data' => $data
+            'data' => $data,
         ]);
     }
 }
