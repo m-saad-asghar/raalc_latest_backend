0242a8538c434fe96c8668c32ea13cecbc26bf70
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-06-17 14:41:42 +0400

    fix condition for organic and non organic

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -136,7 +136,7 @@ class LogController extends Controller
     if ($leadType == 'organic') {
         $query->where(function ($query) {
     $query->where('page_url', 'NOT LIKE', '%utm_campaign%')
-          ->where('page_url', 'NOT LIKE', '%gad_campaignid%');
+          ->orWhere('page_url', 'NOT LIKE', '%gad_campaignid%');
 });
     } elseif ($leadType == 'non_organic') {
    $query->where(function ($query) {
@@ -187,7 +187,7 @@ class LogController extends Controller
     ->where(function ($query) {
         $query->where(function ($query) {
     $query->where('page_url', 'NOT LIKE', '%utm_campaign%')
-          ->where('page_url', 'NOT LIKE', '%gad_campaignid%');
+          ->orWhere('page_url', 'NOT LIKE', '%gad_campaignid%');
 });
         // $query->whereNull('page_url')
         //       ->orWhere('page_url', 'NOT LIKE', '%utm_campaign%');
